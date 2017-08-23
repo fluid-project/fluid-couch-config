57d0ef6368f290e56e5eea1234797e4ab47223e4
Alan Harnum <aharnum@ocadu.ca>
2017-08-23 13:22:04 -0400

    Infusionizing.

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 var fluid = require("infusion");
-var nano = require("nano")('http://localhost:5984');
 
-fluid.defaults("sjrk.server.couchConfig", {
-    gradeNames: "fluid.component",
-    desiredViews: {
-        titles: {
-            map: titleMapFunction
-        },
-        authors: {
-            map: authorMapFunction
-        },
-        tags: {
-            map: tagsMapFunction
-        }
-    }
-})
+var sjrk = fluid.registerNamespace("sjrk");
+
 
 var titleMapFunction = function (doc) {
      emit("title", doc.value.title);
@@ -28,19 +15,45 @@ var tagsMapFunction = function (doc) {
      emit("tags", doc.value.tags);
 };
 
-var desiredViews = {
-    titles: {
-        map: titleMapFunction
-    },
-    authors: {
-        map: authorMapFunction
-    },
-    tags: {
-        map: tagsMapFunction
-    }
+var countMapFunction = function (doc) {
+    emit("count", 1);
 };
 
-var generateViews = function (desiredViews) {
+fluid.defaults("sjrk.server.couchConfig", {
+    gradeNames: "fluid.component",
+    dbConfig: {
+        couchURL: "http://localhost:5984",
+        dbName: "stories"
+    },
+    views: {
+        titles: {
+            map: titleMapFunction
+        },
+        authors: {
+            map: authorMapFunction
+        },
+        tags: {
+            map: tagsMapFunction
+        },
+        count: {
+            map: countMapFunction,
+            reduce: "_count"
+        }
+    },
+    invokers: {
+        generateViews: {
+            funcName: "sjrk.server.couchConfig.generateViews",
+            args: ["{that}.options.views"]
+        },
+        updateViews: {
+            funcName: "sjrk.server.couchConfig.updateViews",
+            args: ["{that}.generateViews", "{that}.options.dbConfig.dbName", "{that}.options.dbConfig.couchURL"]
+        }
+    }
+});
+
+sjrk.server.couchConfig.generateViews = function (desiredViews) {
+    console.log(desiredViews);
     var transformedView = fluid.transform(desiredViews, function (desiredView, viewKey) {
         var transformedFunction = fluid.transform(desiredView, function (viewFunc, funcKey){
             return viewFunc.toString();
@@ -50,24 +63,33 @@ var generateViews = function (desiredViews) {
     return transformedView;
 };
 
-var generatedViews = generateViews(desiredViews);
+sjrk.server.couchConfig.updateViews = function (transformedViewsFunc, dbName, couchURL) {
+    var generatedViews = transformedViewsFunc();
 
-var viewDoc;
+    var viewDoc;
 
-var stories = nano.use("stories");
+    var nano = require("nano")(couchURL);
 
-stories.get("_design/views", function(err, body) {
-    if (!err) {
-        viewDoc = body;
-        console.log(viewDoc);
-        viewDoc.views = generatedViews;
-        console.log(viewDoc);
-        stories.insert(viewDoc, "_design/views", function (err, body) {
-            if(!err) {
-                console.log(body);
-            } else {
-                console.log(err, body);
-            }
-        });
-    }
-});
+    var stories = nano.use(dbName);
+
+    stories.get("_design/views", function(err, body) {
+        if (!err) {
+            viewDoc = body;
+            console.log(viewDoc);
+            viewDoc.views = generatedViews;
+            console.log(viewDoc);
+            stories.insert(viewDoc, "_design/views", function (err, body) {
+                if(!err) {
+                    console.log(body);
+                } else {
+                    console.log(err, body);
+                }
+            });
+        }
+    });
+
+};
+
+var couchConfig = sjrk.server.couchConfig();
+
+couchConfig.updateViews();
